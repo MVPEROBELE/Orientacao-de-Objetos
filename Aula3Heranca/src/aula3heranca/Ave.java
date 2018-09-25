0e7e6eb2758660634100ef34b19a753b00306997
Supervisor <Supervisor@172.18.140.55>
2018-09-24 23:08:01 -0300

    Pokemons aula heranca

---
+++ Aula3Heranca/src/aula3heranca/Ave.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aula3heranca;
+
+/**
+ *
+ * @author Supervisor
+ */
+public class Ave extends Animal{
+    @Override
+    public void emitirSom(){
+        super.emitirSom();
+            System.out.println("psi fi psi f");
+}
+    
+}
